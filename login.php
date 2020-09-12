ba3bc096d22f2e09aab2bb28703f095d1eea96df
unknown <rbenson.arias@gmail.com>
2020-09-12 08:01:00 +0800

    Add validation

--- login.php
+++ login.php
@@ -31,10 +31,43 @@ if (isset($_POST['login'])) {
 
 // Register POST Action
 if(isset($_POST['register'])) {
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    // Empty by default
+    $firstName = "";
+    $lastName = "";
+    $email = "";
+    $password = "";
+
+    // Validation
+    //First Name
+    if(isFirstNameValid($_POST['firstName']) == 1) {
+        $name = formValidate($_POST['firstName']);
+    } else {
+        die("Error: Invalid First Name!");
+    }
+
+     //Last Name
+    if(isNameValid($_POST['lastName']) == 1) {
+        $name = formValidate($_POST['lastName']);
+    } else {
+        die("Error: Invalid Last Name!");
+    }
+
+    // Email
+    if(isEmailValid($_POST['email']) == 1) {
+        $email = formValidate($_POST['email']);
+    } else {
+        die("Error: Invalid Email!");
+    }
+
+    // Password
+    if(isPasswordValid($_POST['password']) == 1) {
+        $password = $_POST['password'];
+    } else {
+        die("Error: Invalid Password!");
+    }
+
+
+    // For duplicate email checking
     $sql = "SELECT * FROM users WHERE email = '$email'";
     $user = $con->query($sql) or die($con->error);
     $row = $user->fetch_assoc();
